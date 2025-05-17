3a7dc8411852f3111252ba956fcd140e2d17aa2b
Mohammad Fayzi <fayzi.dev@gmail.com>
2025-05-17 06:23:01 +0330

    create gradient descent  3D 2 variable

--- gradient_descent.py
+++ gradient_descent.py
@@ -41,4 +41,28 @@ eta = 0.1
 N = 30
 
 gd_3 = gradient_descent(funcx3, xi, eta, N)
-print(gd_3) #tensor(1., requires_grad=True)
+print(gd_3)  # tensor(1., requires_grad=True)
+
+
+# create gradient_descent 3D 2 variable
+def funcx4(x, y):
+    return x * torch.exp(-x ** 2 - y ** 2) + 0.05 * (x ** 2 + y ** 2)
+
+
+def gradient_descent3d(func, xi, yi, xeta, N):
+    for iteration in range(N):
+        func(xi, yi).backward()
+        xi.data -= eta * xi.grad
+        yi.data -= eta * yi.grad
+        xi.grad.zero_()
+        yi.grad.zero_()
+    # return xi.data
+    return xi, yi
+
+xi = torch.tensor(-2., requires_grad=True)
+yi = torch.tensor(2., requires_grad=True)
+eta = 0.1
+N=100
+
+gd_4 = gradient_descent3d(funcx4, xi, yi, yi, N)
+print(gd_4)  #(tensor(-0.6691, requires_grad=True), tensor(0.0089, requires_grad=True))
